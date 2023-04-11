16836562efa26ac76dfcda732519698ef63e499e
Czcibor Bohusz-Dobosz <czbd@o2.pl>
2023-04-11 07:57:03 +0200

    Let's see if that gradually improves it

--- py/krebs/povrayEasy.py
+++ py/krebs/povrayEasy.py
@@ -282,7 +282,7 @@ class EasyPovRayRender(object):
     self.tempfiles = []
 
 
-  def setCamera(self, pos, lookat, fov, projection='perspective', **kwargs):
+  def setCamera(self, pos, lookat, fov, projection=b'perspective', **kwargs):
     W, H = self.params.res
     ratio = float(W)/float(H)
     a = dict(
@@ -461,8 +461,8 @@ class EasyPovRayRender(object):
     vd.name = os.path.splitext(os.path.basename(vd.filename))[0].upper()
     pv.Declare(vd.name, pv.Function(
         pv.Pattern(
-        'density_file df3 "%s"' % vd.filename,
-        'interpolate 1',
+        b'density_file df3 "%s"' % vd.filename,
+        b'interpolate 1',
         ['scale', (wb[:,1]-wb[:,0])],
         ['translate', (wb[:,0])],
       )
